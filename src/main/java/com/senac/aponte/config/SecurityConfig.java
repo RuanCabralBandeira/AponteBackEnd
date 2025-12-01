647a9cb9d0f45b05372b1654c88b38d2dce0900d
Ruan Bandeira <iffygorilla29@gmail.com>
2025-12-01 17:51:38 -0300

    TESTE

--- src/main/java/com/senac/aponte/config/SecurityConfig.java
+++ src/main/java/com/senac/aponte/config/SecurityConfig.java
@@ -75,6 +75,7 @@ public class SecurityConfig {
 
 
                         .requestMatchers("/", "/index.html", "/*.apk", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/photos/**").permitAll()
                         // ----------------------------------
 
                         .anyRequest().authenticated()
