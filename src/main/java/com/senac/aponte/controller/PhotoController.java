1bf43bb7cb44bf079dfbc66cefdcb07be41af123
Ruan Bandeira <iffygorilla29@gmail.com>
2025-12-07 14:11:42 -0300

    commita

--- src/main/java/com/senac/aponte/controller/PhotoController.java
+++ src/main/java/com/senac/aponte/controller/PhotoController.java
@@ -3,15 +3,14 @@ package com.senac.aponte.controller;
 import com.senac.aponte.entity.Photo;
 import com.senac.aponte.entity.Profile;
 import com.senac.aponte.repository.PhotoRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/photos")
@@ -20,32 +19,60 @@ public class PhotoController {
     @Autowired
     private PhotoRepository photoRepository;
 
+
+    @GetMapping("/profile/{profileId}")
+    public ResponseEntity<byte[]> getPhotoByProfile(@PathVariable Integer profileId) {
+
+        Optional<Photo> photoOptional = photoRepository.findFirstByProfileId(profileId);
+
+        if (photoOptional.isPresent()) {
+            Photo photo = photoOptional.get();
+
+
+            MediaType mediaType = MediaType.IMAGE_JPEG;
+            if (photo.getUrl() != null && photo.getUrl().toLowerCase().endsWith(".png")) {
+                mediaType = MediaType.IMAGE_PNG;
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(mediaType)
+                    .body(photo.getPhotoData());
+        } else {
+
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
     @PostMapping("/upload")
     public ResponseEntity<String> uploadImage(
             @RequestParam("file") MultipartFile file,
-            @RequestParam("profileId") Integer profileId) { // Recebe o 7 do Postman
+            @RequestParam("profileId") Integer profileId) {
 
         try {
+
+            Optional<Photo> oldPhoto = photoRepository.findFirstByProfileId(profileId);
+            if (oldPhoto.isPresent()) {
+                photoRepository.delete(oldPhoto.get());
+            }
+
             Photo photo = new Photo();
             photo.setPhotoData(file.getBytes());
             photo.setUrl(file.getOriginalFilename());
             photo.setUploadedAt(LocalDateTime.now());
 
-
             Profile profile = new Profile();
             profile.setId(profileId);
 
             photo.setProfile(profile);
-            // ------------------------
 
             photoRepository.save(photo);
 
             return ResponseEntity.ok("Sucesso! Imagem salva com ID: " + photo.getId());
 
         } catch (Exception e) {
-            e.printStackTrace(); // Isso ajuda a ver o erro no console
+            e.printStackTrace();
             return ResponseEntity.internalServerError().body("Erro: " + e.getMessage());
         }
     }
 }
-
--- src/main/java/com/senac/aponte/repository/PhotoRepository.java
+++ src/main/java/com/senac/aponte/repository/PhotoRepository.java
@@ -4,6 +4,9 @@ import com.senac.aponte.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
+    Optional<Photo> findFirstByProfileId(Integer profileId);
 }
